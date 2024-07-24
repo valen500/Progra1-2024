59b294a42f97aa9e2889246e79c5cf857b852cc3
Valen <brianalvarado078@gmail.com>
2024-07-24 10:22:26 -0600

    ejercicio obtener el promedio de una serie de numeros

---
+++ operadores/operadores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadores_arit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prioridad de los operadores aritmeticos 
+            //ejercicio obtener el promedio de una serie de numeros 
+
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma = suma + num;
+
+                double prom = suma / serie.Length;
+                Console.WriteLine("la suma es: {0}, el promedio {1}", suma, prom);
+                //pausa 
+                Console.ReadLine();
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
+   
