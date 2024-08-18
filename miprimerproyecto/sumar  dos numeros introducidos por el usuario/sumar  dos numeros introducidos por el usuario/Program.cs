5feeba38aee442b69e8cbff67b88a06c113a3cca
Valen <brianalvarado078@gmail.com>
2024-08-18 17:49:09 -0600

    suma de dos numeros introducidos por el usuario

---
+++ miprimerproyecto/sumar  dos numeros introducidos por el usuario/sumar  dos numeros introducidos por el usuario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace miPrimerProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Num1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //Pausa.
+            Console.ReadLine();
+        }
+    }
+}
