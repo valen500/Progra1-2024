e368759ec9cdebb970a47e21176f40e58e1a63f3
Valen <brianalvarado078@gmail.com>
2024-08-18 18:47:51 -0600

    obtener nota final del computo 1, programacion 1

---
+++ miprimerproyecto/nota final computo 1/nota final computo 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nota_final_computo_1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+
+            //ejercicio de obtener la nota final de la materia de Programacion 1
+            Console.Write("Lab1: ");
+            double lab1 = double.Parse(Console.ReadLine()); //8
+
+        Console.Write("Lab2: ");
+            double lab2 = double.Parse(Console.ReadLine()); //9
+
+        Console.Write("Parcial 1: ");
+            double parcial1 = double.Parse(Console.ReadLine()); //7
+
+        // 8*30%= 2.4
+        // 9*30%= 2.7
+        // 7*40%= 2.8
+        //C1    = 7.9
+
+        double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+        Console.WriteLine("La nota de C1 es: {0}", c1);
+            //Pausa.
+            Console.ReadLine();
+        
+        }
+    }
+}
