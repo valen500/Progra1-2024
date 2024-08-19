a5ac76ac98c94973d8949d919e7e7a48e4f36ffe
Valen <brianalvarado078@gmail.com>
2024-08-18 19:16:36 -0600

    if. ejercicio, pedir al usuario la edad si es mayor de edad que le diga bienvenido

---
+++ miprimerproyecto/pedir al usuario si es mayor o menor de edad/pedir al usuario si es mayor o menor de edad/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pedir_al_usuario_si_es_mayor_o_menor_de_edad
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+
+             //estructuras de control.
+             // if. ejercicio, pedir al usuario la edad si es mayor de edad que le diga bienvenido
+        Console.Write("Edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18) {
+                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
+            } else {
+                Console.WriteLine("Eres un adolescente, disfruta de la vida.");
+
+            }
+
+            //Pausa.
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
