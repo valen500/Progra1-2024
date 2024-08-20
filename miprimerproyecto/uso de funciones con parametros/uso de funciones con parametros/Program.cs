1fa1c0d19c2bac63330c42848957067604204492
Valen <brianalvarado078@gmail.com>
2024-08-20 11:02:46 -0600

    uso de funciones con parametros

---
+++ miprimerproyecto/uso de funciones con parametros/uso de funciones con parametros/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uso_de_funciones_con_parametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+ {
+            //Uso de funciones...
+            Console.WriteLine("La suma de 10+5={0}", suma());
+
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
+
+            Console.ReadLine();
+        }
+        static int suma()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
+        }
+        static int suma(int a, int b)
+        {
+            int respuesta = a + b;
+            return respuesta;
+        }
+    }
+}
