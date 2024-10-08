c38b19b619a36ab0d5852988433347cd13340f26
Valen <brianalvarado078@gmail.com>
2024-10-08 17:06:00 -0600

    Creacion de nuestra aplicacion web con ASP.NET Core

---
+++ sistema_academico/sistema_academico/Models/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_academico.Models
+{
+    public class Alumno
+    {
+        [Key]
+        public int idAlumno { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+    }
+}
---
+++ sistema_academico/sistema_academico/Models/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_academico.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Alumno>().HasKey(e => e.idAlumno);
+        }
+    }
+}
